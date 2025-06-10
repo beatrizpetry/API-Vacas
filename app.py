09370da4acbd26fa695468142e8beeaab8d850dd
maricamily <mariana.camily.santos@hotmail.com>
2025-06-10 13:25:01 -0300

    adicionando string de conexão com azure

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flasgger import Swagger
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///vacas.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 swagger = Swagger(app)
